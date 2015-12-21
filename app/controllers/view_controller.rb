7bdb27b959f5f9bcd8c1d613494d85a3c5b7e9eb
Johan Lundgren <johan.emil.lundgren@gmail.com>
2015-12-21 23:32:33 +0100

    init commit

---
+++ app/controllers/view_controller.rb
@@ -0,0 +1,12 @@
+class ViewController < ApplicationController
+
+
+def index
+  @msg = "hello"
+end
+
+def widget1
+  
+end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MirrorApp::Application.config.secret_token = '2b253b7e7563480be5a456498e770b60849ebc4f8e3de7bcc6621d4b8c7d49f935fbac1d78b998e8b14ca5d62b5a3ea3b23445fb00395fe6c7541bdfca7ecad5'
